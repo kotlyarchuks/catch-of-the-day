f382a75f7fc912791706ac4b045fa79af6fd5492
Denis Kotlyarchuk <kotlyarchukd@gmail.com>
2018-07-28 11:16:01 +0200

    Initial commit, structure

---
+++ src/components/StorePicker.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class StorePicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <p>I am the Store Picker!</p>
+     );
+  }
+}
+ 
+export default StorePicker;
