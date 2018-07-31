bfdd48bc88c3a7a2eb5dd7078ee7ed752899cb5a
Denis Kotlyarchuk <kotlyarchukd@gmail.com>
2018-07-31 09:28:18 +0200

    Create Fish component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Test from '../css/images/test.jpg'
+import Fish from './Fish'
 
 class Menu extends Component {
   constructor(props) {
@@ -16,26 +16,7 @@ class Menu extends Component {
             <h2>Fresh Seafood Market</h2>
           </div>
         </div>
-        {/* <!-- Items --> */}
-        <div className="menu__item flex sold-out">
-          <div className="menu__img inline">
-            <img src={Test} alt="" />
-          </div>
-          <div className="menu__content inline">
-            <div className="menu__product-header flex">
-              <h3 className="menu__title">Pacific Halibut</h3>
-              <span className="menu__price">$17.24</span>
-            </div>
-            <div className="menu__desc">
-              Everyones favorite white fish. We will cut it to the size you
-              need and ship it.
-            </div>
-            <div className="menu__btn">
-              <button className="btn">Add to order</button>
-            </div>
-          </div>
-        </div>
-        {/* <!-- End item --> */}
+        <Fish />
       </div>;
   }
 }
